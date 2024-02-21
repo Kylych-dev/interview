fe91a1f2e21c3befaa82cfede441f98b2a6736d1
kylych-bob <mirbekov1kylych@gmail.com>
2024-02-21 13:39:04 +0600

    delete old project

--- apps/lesson_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('userles/', views.user_lessons, name='user_lessons'),
-    # path('user_lessons/', views.user_products, name='user_lessons'),
-    path('prodles/<int:product_id>/', views.lessons_list, name='product_lessons'),
-    path('prodstat/', views.product_statistics, name='product_statistics'),
-]
